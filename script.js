fabd97d583b1a42e8f81df8d07dc837d54d4611b
lidianesouzz <161317758+lidianesouzz@users.noreply.github.com>
2024-07-31 11:45:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+<script type="importmap">
+  {
+    "imports": {
+      "@google/generative-ai": "https://esm.run/@google/generative-ai"
+    }
+  }
+</script>
+async function run() {
+    const prompt = "Write a story about an AI and magic"
+  
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+  
+  run();
